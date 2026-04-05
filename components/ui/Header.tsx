c564ff08895d5f6b8c94e590dbc37ae950c8f1fa
Yashh <yashvardhan241108@gmail.com>
2026-04-05 21:21:07 +0530

    header work

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,3 +1,4 @@
+import { shadow } from "@/styles/utils"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -5,7 +6,13 @@ import Link from "next/link"
 
 function Header() {
   return (
-    <header><Link href="/">
+    <header className="bg-popover relative flex h-24 w-full item-center justify-between px-3 sm:px-8 "
+    
+     style={{
+        boxShadow:shadow, 
+    }}>
+   
+        <Link href="/" className="flex items-end gap-2">
             <Image src="/images.png" 
             height={60} 
             width={60} 
@@ -13,6 +20,8 @@ function Header() {
             className="rounded-full" 
             priority
             />
+
+            <h1 className="flex flex-col pb-1 text-2xl font semibold loading-6">Sky<span>Lark</span></h1>
 
     </Link>
 
